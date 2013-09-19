234a88a04ce750eeee450102f35e0fbe197bf82f
buildguy <buildguy@pentaho.com>
2013-09-19 15:55:09 +0000

    [PPP-3081] license header updates on all .java, .js, and .jsp

--- src/org/pentaho/di/palo/core/ConsolidationElement.java
+++ src/org/pentaho/di/palo/core/ConsolidationElement.java
@@ -1,21 +1,21 @@
 /*
- *   This file is part of PaloKettlePlugin.
- *
- *   PaloKettlePlugin is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU Lesser General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   PaloKettlePlugin is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU Lesser General Public License for more details.
- *
- *   You should have received a copy of the GNU Lesser General Public License
- *   along with PaloKettlePlugin.  If not, see <http://www.gnu.org/licenses/>.
- *
- *   Copyright 2012 De Bortoli Wines Pty Limited (Australia)
- */
+*   This file is part of PaloKettlePlugin.
+*
+*   PaloKettlePlugin is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU Lesser General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   PaloKettlePlugin is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU Lesser General Public License for more details.
+*
+*   You should have received a copy of the GNU Lesser General Public License
+*   along with PaloKettlePlugin.  If not, see <http://www.gnu.org/licenses/>.
+*
+*   Copyright 2012 De Bortoli Wines Pty Limited (Australia)
+*/
 
 package org.pentaho.di.palo.core;
 
